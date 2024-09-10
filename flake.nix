7c50d1e65b05a9c820a35ba235dc1b0a96837cbb
Michael Smith <m@michaelsmith.be>
2024-09-10 21:30:03 +0200

    Add devshell serve command. Format nix files

--- flake.nix
+++ flake.nix
@@ -11,9 +11,13 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, devshell }:
-
-    let forAllSystems = function:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    devshell,
+  }: let
+    forAllSystems = function:
       nixpkgs.lib.genAttrs [
         "x86_64-linux"
         "x86_64-darwin"
@@ -26,52 +30,58 @@
             devshell.overlays.default
           ];
         }));
-      in
-      {
-        formatter = forAllSystems (pkgs: pkgs.alejandra);
+  in {
+    formatter = forAllSystems (pkgs: pkgs.alejandra);
 
-        devShells = forAllSystems (pkgs: {
-          default = pkgs.devshell.mkShell {
-            name = "nixup-marketing-website";
-            packages = let
-              gems = pkgs.bundlerEnv rec {
-                name = "nixup-marketing-website-gems";
-                ruby = pkgs.ruby_3_2;
-                gemdir = ./.;
-              };
-            in [
-              gems
-              (pkgs.lowPrio gems.wrappedRuby)
-              pkgs.rsync
-            ];
-
-            env = [
-              {
-                name = "BUNDLE_FORCE_RUBY_PLATFORM";
-                eval = "1";
-              }
-            ];
-            commands = [
-              {
-                name = "update:gems";
-                category = "Dependencies";
-                help = "Update `Gemfile.lock` and `gemset.nix`";
-                command = ''
-                  ${pkgs.ruby_3_2}/bin/bundle lock
-                  ${pkgs.bundix}/bin/bundix
-                '';
-              }
-              {
-                name = "upgrade:gems";
-                category = "Dependencies";
-                help = "Update `Gemfile.lock` and `gemset.nix`";
-                command = ''
-                  ${pkgs.ruby_3_2}/bin/bundle lock --update
-                  ${pkgs.bundix}/bin/bundix
-                '';
-              }
-            ];
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.devshell.mkShell {
+        name = "nixup-marketing-website";
+        packages = let
+          gems = pkgs.bundlerEnv rec {
+            name = "nixup-marketing-website-gems";
+            ruby = pkgs.ruby_3_2;
+            gemdir = ./.;
           };
-        });
+        in [
+          gems
+          (pkgs.lowPrio gems.wrappedRuby)
+          pkgs.rsync
+        ];
+
+        env = [
+          {
+            name = "BUNDLE_FORCE_RUBY_PLATFORM";
+            eval = "1";
+          }
+        ];
+        commands = [
+          {
+            name = "update:gems";
+            category = "Dependencies";
+            help = "Update `Gemfile.lock` and `gemset.nix`";
+            command = ''
+              ${pkgs.ruby_3_2}/bin/bundle lock
+              ${pkgs.bundix}/bin/bundix
+            '';
+          }
+          {
+            name = "upgrade:gems";
+            category = "Dependencies";
+            help = "Update `Gemfile.lock` and `gemset.nix`";
+            command = ''
+              ${pkgs.ruby_3_2}/bin/bundle lock --update
+              ${pkgs.bundix}/bin/bundix
+            '';
+          }
+          {
+            name = "serve";
+            help = "Run a server hosting the website locally";
+            command = ''
+              jekyll server --livereload
+            '';
+          }
+        ];
       };
+    });
+  };
 }
